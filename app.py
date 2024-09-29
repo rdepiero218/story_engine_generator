a19e6da743c77ea15d75ea1b05c647982127b4a7
Regene DePiero <regenedepiero@gmail.com>
2024-09-29 18:48:18 -0400

    some minor updates

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ card_sets = {
 
 st.set_page_config(page_title="Story Engine", page_icon="", layout='wide')
 st.title('Story Engine Generator')
-st.markdown('A basic digital version of the story engine card deck.')
+st.markdown('A super basic digital version of the story engine card deck.')
 
 
 col1, col2 = st.columns([1,1])
@@ -26,7 +26,7 @@ with col1:
 with col2:
     n_stacks = st.number_input('How many stacks do you want?', min_value=1, max_value=10, value=3)
 
-st.write('Your batch includes ', n_stacks, ' stacks from the ', card_set_choice, ' deck.')
+st.write('Your batch will have ', n_stacks, ' stacks from the ', card_set_choice, ' deck.')
 
 filename = 'story_generator'
 
@@ -37,12 +37,13 @@ else:
 
 se.generate_card_batch_file(card_set, n_stacks, filename, engine_type)
 
-st.text(open('story_generator.txt', 'r+').read())
+if st.button('Generate!'):
+    st.text(open('story_generator.txt', 'r+').read())
 
-with open('story_generator.txt', 'r+') as file:
-     btn = st.download_button(
-             label='Download Output as Text File',
-             data=file,
-             file_name='story_generator',
-             mime='text/'
-           )
+    with open('story_generator.txt', 'r+') as file:
+        btn = st.download_button(
+                label='Download Output as Text File',
+                data=file,
+                file_name='story_generator',
+                mime='text/'
+            )
